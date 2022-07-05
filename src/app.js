2526b1171c0e9c33f312d6f6fab678d66e5c4a83
Gerald Vargas <geraldfvv@live.com>
2022-07-05 11:47:47 -0600

    deploy test

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://bn-immersive.herokuapp.com/"],
+    origin: "https://bn-immersive.herokuapp.com/",
     credentials: true,
   })
 );
